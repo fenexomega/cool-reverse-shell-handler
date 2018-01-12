6622c60bd18d2b40d0001b4b21f5d43c4b08873c
jordy <jordy@jordy>
2018-01-12 09:22:34 -0300

    update

--- handler/simplesocket.py
+++ handler/simplesocket.py
@@ -8,51 +8,70 @@ import time
 TIME_TO_WAIT = 0.1
 
 class simplesocket(threading.Thread):
-
-    def __init__(self,ip,port,conn,observers):
+    def __init__(self,id,ip,port,conn,observers,conns_list):
         super().__init__()
-        self.ip = ip
-        self.port = port
-        self.observers = observers
-        self.tcp = conn
-        self.online = True
+        self.ip         = ip
+        self.port       = port
+        self.observers  = observers
+        self.tcp        = conn
+        self.online     = True
+        self.conns_list = conns_list
+        self.id         = id 
+        self.out_queue  = []
 
     def sendall(self,message):
         self.tcp.sendall(message.encode())
 
+    def notify(message):
+        for o in self.observers:
+            o.notify(id,message)
+
+    def notify_offline():
+        for o in self.observers:
+            o.notify_offline(self)
+
+    def sendall(message):
+        self.out_queue.append(message)
+
     def run(self):
         con_list = [self.tcp]
-        i = 0
         try:
-            loop = True
-            while loop:
+            while self.online:
                 print("WAITING INPUT")
                 time.sleep(TIME_TO_WAIT)
-                if self.conn.fileno() == -1 or not self.online:
+                if  not self.online:
                     break
                 else:
-                    senders,receivers, errors_rec = \
+                    r,s,e = \
                         select.select(con_list,con_list,[],TIMEOUT)
-                    for r in receivers:
+                    for c in s:
                         for msg in self.out_queue:
-                            r.sendall(msg.encode())
+                            c.sendall(msg.encode())
                         self.out_queue.clear()
-                    for s in senders:
-                            msg = s.recv(BUFFER).decode('utf8')
+                    for c in r:
+                            msg = c.recv(BUFFER).decode('utf8')
                             if msg:
-                                self.execute_cmd(msg)
+                                self.notify(msg)
                             else:
-                                loop = False
+                                self.shutdown()
+                                self.online = False
+                                break
         except Exception as e:
             print(e)
         finally:
             if self.online:
-                self.stop_connection()
+                self.shutdown()
             self.remove_itself()
-
+    
+    def shutdown(self):
+        self.online = False
+        self.tcp.shutdown(socket.SHUT_RDWR)
+        self.tcp.close()
+        self.notify_offline()
 
     def close(self):
-
+        if self.online:
+            self.shutdown()
         self.join()
         pass
 
