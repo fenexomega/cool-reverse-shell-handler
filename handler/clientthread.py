64cd225b8ec9a7a999333478d9738c02e41bea9a
Jordyf <jordyfgomes@gmail.com>
2018-05-27 21:25:47 -0300

    update

--- handler/clientthread.py
+++ handler/clientthread.py
@@ -39,32 +39,33 @@ class ClientThread(threading.Thread):
                         'ip': shell.ip}
                 l.append(obj)
         obj = {'messageType':4,'content':{'title':'connected_clients','content':l}}
-        self.send_in_json(obj)
+        self.send_as_json(obj)
 
     def execute_cmd(self,cmd):
         #EXECUTE COMMAND
         try:
             print(cmd)
-            self.shell_id = cmd['id']
-            shell = self.shells[self.shell_id]
+            shell_id = cmd['id']
+            shell = self.shells[shell_id]
             if not shell.online:
-                self.send_in_json('Error: That connection is closed')
+                self.send_as_json('Error: That connection is closed')
                 return
             shell.sendall(cmd['cmd'])
             output = shell.recv()
-            self.send_in_json(output)
+            cmd = {'messageType':'response','content':{'id':shell_id,'message':output}}
+            self.send_as_json(cmd)
         except IndexError:
-            self.send_in_json('Error: That connection doesn\'t exist')
+            self.send_as_json('Error: That connection doesn\'t exist')
 
 
-    def send_in_json(self,obj):
+    def send_as_json(self,obj):
         print(obj)
         message = json.dumps(obj)
         self.out_queue.append(message)
 
     def notify(self,message):
        msg = json.dumps(message)
-       self.send_in_json(msg)
+       self.send_as_json(msg)
 
     def run(self):
         con_list = [self.conn]
